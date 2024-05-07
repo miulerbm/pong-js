02e395681f0ddab1135a836b666cdbd17fe10033
miulerbm <miulerbm00@gmail.com>
2024-05-07 17:01:06 -0500

    Collisions

--- pong.js
+++ pong.js
@@ -6,7 +6,7 @@ let context;
 
 // players
 let playerWidth = 10;
-let playerHeight = 50;
+let playerHeight = 70;
 let playerVelocityY = 0;
 
 let player1 = {
@@ -23,6 +23,18 @@ let player2 = {
   width: playerWidth,
   height: playerHeight,
   velocityY: playerVelocityY,
+};
+
+//ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ball = {
+  x: boardWith / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: 2,
+  velocityY: 4,
 };
 
 window.onload = function () {
@@ -54,6 +66,7 @@ function update() {
   let nextPlayer1Y = player1.y + player1.velocityY;
   if (!outOfBounds(nextPlayer1Y)) {
     player1.y = nextPlayer1Y;
+    player1.velocityY = 0;
   }
   context.fillRect(player1.x, player1.y, player1.width, player1.height);
 
@@ -63,8 +76,34 @@ function update() {
   let nextPlayer2Y = player2.y + player2.velocityY;
   if (!outOfBounds(nextPlayer2Y)) {
     player2.y = nextPlayer2Y;
+    player2.velocityY = 0;
   }
   context.fillRect(player2.x, player2.y, player2.width, player2.height);
+
+  // ball
+  context.fillStyle = "white";
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  // if ball touches top or bottom of canvas
+  if (ball.y <= 0 || ball.y + ball.height >= boardHeight) {
+    // reverse ball course
+    ball.velocityY *= -1; //reverse direction
+  }
+
+  // bounce the ball back
+  if (detectCollision(ball, player1)) {
+    if (ball.x <= player1.x + player1.width) {
+      // left side of ball touches right side of player1
+      ball.velocityX *= -1; //flip x direction
+    }
+  } else if (detectCollision(ball, player2)) {
+    if (ball.x + ballWidth >= player2.x) {
+      //right side of ball touches left side of player2
+      ball.velocityX *= -1; //flip x direction
+    }
+  }
 }
 
 function outOfBounds(yPosition) {
@@ -76,15 +115,24 @@ function movePlayer(e) {
   //player1
   if (e.code === "KeyW") {
     // Negative velocity goes up because top left is (0,0)
-    player1.velocityY = -4;
+    player1.velocityY = -35;
   } else if (e.code === "KeyS") {
-    player1.velocityY = 4;
+    player1.velocityY = 35;
   }
 
   if (e.code === "ArrowUp") {
     // Negative velocity goes up because top left is (0,0)
-    player2.velocityY = -4;
+    player2.velocityY = -35;
   } else if (e.code === "ArrowDown") {
-    player2.velocityY = 4;
+    player2.velocityY = 35;
   }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width && // a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && //a's top right corner passes b's top left corner
+    a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
+    a.y + a.height > b.y
+  ); // a's bottom left corner passes b's top left corner
 }
